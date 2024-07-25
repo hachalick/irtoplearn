943edae012173cc25140ee2a351ea4fbf9378ac3
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-25 18:06:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from classes.readweb import ReadWeb
+from classes.readweb import ReadWeb as rweb
+from classes.ir import irsystem as irs
 
-a = ReadWeb()
-a.run()
+# max limit test = 10
+limit = 10
+# obj_rweb = rweb(limit)
+# obj_rweb.run()  # run just for first time
+#
+obj_irs = irs(limit)
+# obj_irs.normalizer_data()
+# obj_irs.create_posting_list()
