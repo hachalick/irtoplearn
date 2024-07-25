46a5faab91c614ca189af33aa8c3d433195ad723
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-07-25 18:06:55 +0330

    Create namefile.py

---
+++ classes/namefile.py
@@ -0,0 +1,7 @@
+class NameFile:
+    data_website_json = "data_website"
+    links_json = "links"
+    dataset_json = "dataset"
+    list_all_word_json = "list_all_word"
+    posting_list_json = "posting_list"
+    courses_xml = "courses"
